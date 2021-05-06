a87f112e261d36a2bcf42f5a9f71d72cd05c00ce
christ5013 <christineGodinez.rubio.5013@gmail.com>
2021-05-06 20:09:15 +0800

    CRUD

---
+++ CRUDemployee/src/crudemployee/CRUDemployee.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crudemployee;
+
+/**
+ *
+ * @author 2ndyrGroupA
+ */
+public class CRUDemployee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new AddEmployee().setVisible(true);
+  
+    }
+    
+}
